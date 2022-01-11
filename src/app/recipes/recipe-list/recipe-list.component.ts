8564d4ea01b42aefabdcd32b3c399569454ad605
trudykush <kushalmishra91@gmail.com>
2022-01-11 23:17:44 +0000

    Recipe list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,10 @@ import { Recipe } from '../recipe.model'
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('Test Recipe', 'This is description', 'https://foxeslovelemons.com/wp-content/uploads/2021/09/Mashed-Potato-Cakes-Potato-Cake-Recipe-3.jpg')
+    new Recipe('Test Recipe', 'This is description',
+      'https://media.istockphoto.com/photos/culinary-background-with-spices-and-recipe-book-picture-id607299402?k=20&m=607299402&s=612x612&w=0&h=kyjzviV27vVh7UpW1gj_jUm26umMdS17NJvhz4wls8s='),
+      new Recipe('Second Recipe', 'This is second description',
+            'https://www.maggi.co.uk/sites/default/files/styles/maggi_desktop_image_style/public/NUK1265%20maggi%20Recipes%20banner%201500x700px%20opt2A.jpg?h=4f5b30f1&itok=DcsF1RwA')
   ];
 
   constructor() { }
