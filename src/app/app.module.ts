0684e629330b184ba23031ccbaa6bd64a2486012
trudykush <kushalmishra91@gmail.com>
2022-01-13 22:52:36 +0000

    New App structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { CockpitComponent } from './cockpit/cockpit.component';
+import { ServerElementComponent } from './server-element/server-element.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CockpitComponent,
+    ServerElementComponent
   ],
   imports: [
     BrowserModule,
@@ -17,3 +21,51 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+
+
+
+
+
+
+
+/*
+ import { BrowserModule } from '@angular/platform-browser';
+ import { NgModule } from '@angular/core';
+ import { FormsModule } from '@angular/forms';
+
+
+ import { AppComponent } from './app.component';
+ import { HeaderComponent } from './header/header.component';
+ import { RecipesComponent } from './recipes/recipes.component';
+ import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+ import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+ import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+
+ @NgModule({
+   declarations: [
+     AppComponent,
+     HeaderComponent,
+     RecipesComponent,
+     RecipeListComponent,
+     RecipeDetailComponent,
+     RecipeItemComponent,
+     ShoppingListComponent,
+     ShoppingEditComponent
+     AppComponent
+   ],
+   imports: [
+     BrowserModule,
+     FormsModule
+     FormsModule,
+   ],
+   providers: [],
+   bootstrap: [AppComponent]
+ })
+ export class AppModule { }
+
+
+ */
